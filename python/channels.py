5b8f2db983fbac1cb1bc31161ab709e565dd25b3
me <me@me.me>
2026-02-16 13:20:23 -0800

    Channels information.

---
+++ python/channels.py
@@ -0,0 +1,9 @@
+bluetooth_channels = [32, 34, 46, 48, 50, 52, 0, 1, 2, 4, 6, 8, 22, 24, 26, 28, 30, 74, 76, 78, 80]
+ble_channels       = [2, 26, 80]
+WiFi_channels      = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+usbWireless_channels = [40, 50, 60]
+videoTransmitter_channels = [70, 75, 80]
+rc_channels        = [1, 3, 5, 7]
+zigbee_channels    = [11, 15, 20, 25]
+nrf24_channels     = [76, 78, 79]
+
